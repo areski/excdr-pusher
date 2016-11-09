d5e17bcd5972821cb426d092d29021de14381cd2
areski <areski@gmail.com>
2016-11-09 16:35:25 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCdrPusher.Mixfile do
 
   def project do
     [app: :excdr_pusher,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
