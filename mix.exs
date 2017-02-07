a90af0c05c213122e25b14069e5b6a5a8c654ff8
areski <areski@gmail.com>
2017-02-07 19:06:49 +0100

    [fix](app) use automatic application detection from elixir v1.4

--- mix.exs
+++ mix.exs
@@ -8,10 +8,8 @@ defmodule ExCdrPusher.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
-
-     # hex
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
     ]
   end
 
@@ -19,20 +17,11 @@ defmodule ExCdrPusher.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :swab, :sqlitex, :instream, :postgrex, :ecto, :timex, :logger_file_backend, :timex_ecto],
-     mod: {ExCdrPusher, []}]
+    [mod: {ExCdrPusher, []},
+     extra_applications: [:logger]]
   end
 
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  # Dependencies
   defp deps do
     [
      {:ex_doc, "~> 0.14.5", only: :dev},
